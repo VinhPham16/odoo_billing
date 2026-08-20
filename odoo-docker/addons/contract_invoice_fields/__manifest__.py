92d47163ae386c19bb8acec76a9fdbd94eaee95c
vinhph <hoangvinh160401@gmail.com>
2026-08-20 17:24:06 +0700

    update_resposity

---
+++ odoo-docker/addons/contract_invoice_fields/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Contract Info on Invoice',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Thêm thông tin hợp đồng vào hoá đơn',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
