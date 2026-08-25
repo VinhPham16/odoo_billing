06bc1ec1af0a3a5b00cce77669ef160677c9059f
vinhph <hoangvinh160401@gmail.com>
2026-08-25 16:12:17 +0700

    update module filter partner là company

---
+++ odoo-docker/addons/account_partner_company_filter/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Invoice Partner Company Filter',
+    'summary': 'Chỉ cho chọn khách hàng là công ty (is_company) trên hóa đơn bán, không ảnh hưởng vendor bill',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting/Accounting',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_view.xml',
+    ],
+    'license': 'LGPL-3',
+}
